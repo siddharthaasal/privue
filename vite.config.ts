992149b71816f14eb26634771ce3acc49b115ccd
Siddharth Aasal <siddharthaasal@gmail.com>
2025-09-29 17:01:29 +0530

    better chumking

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,18 @@ import mdx from '@mdx-js/rollup';
 import rehypeSlug from "rehype-slug";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import remarkFrontmatter from "remark-frontmatter";
+import { visualizer } from "rollup-plugin-visualizer";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), mdx({
+  plugins: [react(), tailwindcss(),
+  visualizer({                 // ✅ adds visual analysis
+    filename: 'stats.html',
+    open: true,                // auto opens after build
+    gzipSize: true,
+    brotliSize: true
+  }),
+  mdx({
     rehypePlugins: [
       rehypeSlug,
       [rehypeAutolinkHeadings, { behavior: 'wrap' }],
@@ -20,6 +28,22 @@ export default defineConfig({
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+    },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('react-dom')) return 'react-dom';
+            if (id.includes('react')) return 'react';
+            if (id.includes('framer-motion')) return 'framer';
+            if (id.includes('react-icons')) return 'icons';
+            if (id.includes('recharts')) return 'recharts';
+            if (id.includes('reactflow')) return 'reactflow';
+          }
+        },
+      },
     },
   },
 })

fc68c44841e5335ce2caad457146d98911dce3fe
Siddharth Aasal <siddharthaasal@gmail.com>
2025-09-29 19:51:28 +0530

    resolve react-reactdom single build

--- vite.config.ts
+++ vite.config.ts
@@ -8,36 +8,47 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import remarkFrontmatter from "remark-frontmatter";
 import { visualizer } from "rollup-plugin-visualizer";
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(),
-  visualizer({                 //  adds visual analysis
-    filename: 'stats.html',
-    open: false,                // auto opens after build
-    gzipSize: true,
-    brotliSize: true
-  }),
-  mdx({
-    rehypePlugins: [
-      rehypeSlug,
-      [rehypeAutolinkHeadings, { behavior: 'wrap' }],
-    ],
-    remarkPlugins: [remarkFrontmatter],
-    include: /\.mdx?$/,
-  })],
+  plugins: [
+    react(),
+    tailwindcss(),
+    visualizer({
+      filename: 'stats.html',
+      open: false,
+      gzipSize: true,
+      brotliSize: true
+    }),
+    mdx({
+      rehypePlugins: [
+        rehypeSlug,
+        [rehypeAutolinkHeadings, { behavior: 'wrap' }],
+      ],
+      remarkPlugins: [remarkFrontmatter],
+      include: /\.mdx?$/,
+    })
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
+      // force both to the top-level node_modules
       react: path.resolve(__dirname, 'node_modules/react'),
+      'react-dom': path.resolve(__dirname, 'node_modules/react-dom'),
     },
+    // prefer a single copy when multiple packages depend on react
+    dedupe: ['react', 'react-dom']
+  },
+  optimizeDeps: {
+    // help vite pre-bundle these explicitly
+    include: ['react', 'react-dom']
   },
   build: {
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react-dom')) return 'react-dom';
-            if (id.includes('react')) return 'react';
+            // if (id.includes('react-dom')) return 'react-dom';
+            // if (id.includes('react')) return 'react';
+            if (id.includes('react') || id.includes('react-dom')) return 'react-vendor';
             if (id.includes('framer-motion')) return 'framer';
             if (id.includes('react-icons')) return 'icons';
             if (id.includes('recharts')) return 'recharts';
